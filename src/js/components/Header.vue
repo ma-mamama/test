1560f0f9d72032c425a811d9bae76ff408f7d28f
ma-mamama <mamamamam.2525@gmail.com>
2022-10-04 00:41:04 +0900

    メンバー一覧画面作成

---
+++ src/js/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="header">
+        <div>FFF</div>
+        <div class="header-title">- {{headerTitle}} -</div>
+        <div></div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "headerTitle",
+    data: () => ({
+        headerTitle: "member"
+    })
+}
+</script>
